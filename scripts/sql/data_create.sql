4dd968d75d3d4abd23d0445defc3661fb15a9592
avi <a0548589558@gmail.com>
2025-08-14 16:01:40 +0300

    added sql script files for creating a table and inserting to the table

---
+++ scripts/sql/data_create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS data (
+    id INT PRIMARY KEY auto_increment,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL
+);
---
+++ scripts/sql/data_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO data (first_name, last_name) VALUES
+('Avi', 'Goldberg'),
+('Yaakov', 'Herzog'),
+('Miriam', 'Levi'),
+('David', 'Cohen'),
+('Rachel', 'Friedman')
